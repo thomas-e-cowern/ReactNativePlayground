62fcce29aa3f2730ed17d7ef146af8f67fa24fc6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-28 18:24:47 -0500

    add icon to App.js

--- UberCloneTutorial/UberCloneTutorial/App.js
+++ UberCloneTutorial/UberCloneTutorial/App.js
@@ -1,9 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Icon name={"rocket"} size={30}></Icon>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
