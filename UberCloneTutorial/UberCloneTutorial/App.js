d1530106a03017ed371163e4373be404a2678ccb
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-28 18:31:30 -0500

    cleared out App.js

--- UberCloneTutorial/UberCloneTutorial/App.js
+++ UberCloneTutorial/UberCloneTutorial/App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Icon name={"rocket"} size={30}></Icon>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark-content" />
     </View>
   );
 }
