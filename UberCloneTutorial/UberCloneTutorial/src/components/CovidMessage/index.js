e0d11ef35f5c2ac93ca7c69056c3e129b4bed189
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-29 09:58:05 -0500

    move styles to CovidMessage

--- UberCloneTutorial/UberCloneTutorial/src/components/CovidMessage/index.js
+++ UberCloneTutorial/UberCloneTutorial/src/components/CovidMessage/index.js
@@ -1,23 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import styles from "./styles";
 
 export default function App() {
   return (
-    <View
-      style={{
-        height: 300,
-        backgroundColor: "#a0abff",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Travel only if necessary</Text>
-      <Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Travel only if necessary</Text>
+      <Text style={styles.text}>
         Help us to restrict the spread of COVID by taking into accout local
         regulations and masking when appropriate
       </Text>
-      <Text>Learn more </Text>
+      <Text style={styles.learnMore}>Learn more </Text>
     </View>
   );
 }
---
+++ UberCloneTutorial/UberCloneTutorial/src/components/CovidMessage/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {},
+  title: {},
+  text: {},
+  learnMore: {},
+});
+
+export default styles;
