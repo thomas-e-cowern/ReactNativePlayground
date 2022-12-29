0c052cd72e995bbdbfeb9ba08b6a1250deac712c
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-29 13:38:11 -0500

    update styles for HomeSearch

--- UberCloneTutorial/UberCloneTutorial/src/components/HomeSearch/index.js
+++ UberCloneTutorial/UberCloneTutorial/src/components/HomeSearch/index.js
@@ -1,23 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
+import styles from "./styles";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 export default function HomeSearch() {
   return (
-    <View
-      style={{
-        height: 300,
-        backgroundColor: "#a0abff",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       {/* Input box */}
-      <View>
-        <Text>Where to?</Text>
-        <View>
+      <View style={styles.inputBox}>
+        <Text style={styles.inputText}>Where to?</Text>
+        <View style={styles.timeContainer}>
           <AntDesign name={"clockcircle"} size={16} />
           <Text>Now</Text>
           <MaterialIcons name={"keyboard-arrow-down"} size={16} />
--- UberCloneTutorial/UberCloneTutorial/src/components/HomeSearch/styles.js
+++ UberCloneTutorial/UberCloneTutorial/src/components/HomeSearch/styles.js
@@ -1,7 +1,27 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.createElement({
-  inputBox: {},
-  inputText: {},
-  timeContainer: {},
+const styles = StyleSheet.create({
+  inputBox: {
+    backgroundColor: "#d9d9d9",
+    margin: 10,
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  inputText: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#434343",
+  },
+  timeContainer: {
+    flexDirection: "row",
+    width: 100,
+    justifyContent: "space-between",
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 50,
+  },
 });
+
+export default styles;
