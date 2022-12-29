820ac5e5a3c9c85eec9cec999c33656b23b5f495
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-29 09:07:46 -0500

    add HomeMap and HomeScreen folders and index.js for each

---
+++ UberCloneTutorial/UberCloneTutorial/src/components/HomeMap/index.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>notJust a component</Text>
+    </View>
+  );
+}
