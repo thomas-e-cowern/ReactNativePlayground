830e04367ea7d1527de3c136a4def0f2dae1f7c2
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-29 11:24:35 -0500

    add homeSearch folder and index.js

---
+++ UberCloneTutorial/UberCloneTutorial/src/components/HomeSearch/index.js
@@ -0,0 +1,18 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        height: 300,
+        backgroundColor: "#a0abff",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>I am a map</Text>
+    </View>
+  );
+}
