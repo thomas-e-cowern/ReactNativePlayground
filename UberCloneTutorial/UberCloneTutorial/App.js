da28d66afef92e623b11fae4c2ace10c849522c8
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-29 09:16:12 -0500

    update App.js with HomeScreen component

--- UberCloneTutorial/UberCloneTutorial/App.js
+++ UberCloneTutorial/UberCloneTutorial/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import HomeScreen from "src/HomeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="dark-content" />
-    </View>
+    // <View style={styles.container}>
+    //   <StatusBar style="dark-content" />
+    // </View>
+
+    <HomeScreen />
   );
 }
 
--- UberCloneTutorial/UberCloneTutorial/src/screens/HomeScreen/index.js
+++ UberCloneTutorial/UberCloneTutorial/src/screens/HomeScreen/index.js
@@ -1,11 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import HomeMap from "../components/HomeMap";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>notJust a component</Text>
+      <HomeMap />
+
+      {/* Covid Message */}
+
+      {/* Bottom Component */}
     </View>
   );
 }
