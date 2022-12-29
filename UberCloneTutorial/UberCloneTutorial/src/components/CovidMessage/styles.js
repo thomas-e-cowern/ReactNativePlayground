a343a22faee3bbb875922fdccc729f1582f0c853
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-29 10:04:58 -0500

    update styles for CovidMessage

--- UberCloneTutorial/UberCloneTutorial/src/components/CovidMessage/styles.js
+++ UberCloneTutorial/UberCloneTutorial/src/components/CovidMessage/styles.js
@@ -1,10 +1,28 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {},
-  title: {},
-  text: {},
-  learnMore: {},
+  container: {
+    backgroundColor: "#2b80ff",
+    padding: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  title: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  text: {
+    color: "white",
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  learnMore: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
 });
 
 export default styles;
