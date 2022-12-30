ae79512dbe563bd3db534d5c94eff0fe3c487769
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-30 06:28:19 -0500

    update styles for previous destination

--- UberCloneTutorial/UberCloneTutorial/src/components/HomeSearch/styles.js
+++ UberCloneTutorial/UberCloneTutorial/src/components/HomeSearch/styles.js
@@ -22,6 +22,23 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderRadius: 50,
   },
+  row: {
+    flexDirection: "row",
+    padding: 10,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#b3b3b3",
+  },
+  iconContainer: {
+    backgroundColor: "#b3b3b3",
+    padding: 10,
+    borderRadius: 25,
+  },
+  destinationText: {
+    marginLeft: 10,
+    fontWeight: "500",
+    fontSize: 16,
+  },
 });
 
 export default styles;
