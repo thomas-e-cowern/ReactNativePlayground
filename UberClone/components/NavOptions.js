989b367943facf62b0b1607481c40aa30be9605c
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-29 14:31:48 -0500

    add data and FlatList to NavOptions

--- UberClone/components/NavOptions.js
+++ UberClone/components/NavOptions.js
@@ -1,11 +1,32 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a Ride",
+    image: "https:links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order Food",
+    image: "https:links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
 
 const NavOptions = () => {
   return (
-    <View>
-      <Text>NavOptions</Text>
-    </View>
+    <FlatList
+      data={data}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity>
+          <Text>{item.title}</Text>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
--- UberClone/screens/HomeScreen.js
+++ UberClone/screens/HomeScreen.js
@@ -1,14 +1,17 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import logo from "../assets/rose-trolley-logo.png";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
-    <View style={{ padding: 5 }}>
+    <View style={{ padding: 10 }}>
       <Image
         style={{ width: 100, height: 100, resizeMode: "contain" }}
         source={logo}
       />
+
+      <NavOptions />
     </View>
   );
 };
