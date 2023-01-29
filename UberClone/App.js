ba15fbe2287dac7ca2ac89daf16603926005f78d
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-29 12:16:50 -0500

    update to fix tailwind error

--- UberClone/App.js
+++ UberClone/App.js
@@ -3,14 +3,13 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
-import tw from "tailwindcss-react-native";
 
 export default function App() {
   return (
     <Provider store={store}>
       <SafeAreaView style={styles.container}>
-        {/* <HomeScreen /> */}
-        <text style={tw`text-red-500`}>Check out Tailwind</text>
+        <HomeScreen />
+        <StatusBar style="auto" />
       </SafeAreaView>
     </Provider>
   );
