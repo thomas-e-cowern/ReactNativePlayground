46cf2aa0485e9724ab71fa99dadfdcbe8a53f629
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-29 11:12:19 -0500

    update App.js and HomeScreen to fix formatting

--- UberClone/App.js
+++ UberClone/App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build Uber 👍 😄!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView style={styles.container}>
+        <HomeScreen />
+      </SafeAreaView>
     </Provider>
   );
 }
