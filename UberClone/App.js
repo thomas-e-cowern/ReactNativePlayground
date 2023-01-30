e08687dc4d0aa6636b72adc2a4e9bdbaaff8ba6b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-30 09:52:32 -0500

    update App.js with StackNavigator

--- UberClone/App.js
+++ UberClone/App.js
@@ -4,12 +4,19 @@ import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaView>
+          <Stack.Navigator
+            name="HomeScreen"
+            component={HomeScreen}
+          ></Stack.Navigator>
           <HomeScreen />
           <StatusBar style="auto" />
         </SafeAreaView>
