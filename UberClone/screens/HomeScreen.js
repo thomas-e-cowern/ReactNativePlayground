4aa98260fd3752650d8d811c31f13ee3c42c5545
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-01-31 10:07:24 -0500

    add GooglePlacesAutocomplete to HomeScreen

--- UberClone/screens/HomeScreen.js
+++ UberClone/screens/HomeScreen.js
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import logo from "../assets/rose-trolley-logo.png";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+// import { GOOGLE_MAPS_APIKEY } from "@env";
+import Config from "react-native-config";
 
 const HomeScreen = () => {
   return (
@@ -9,6 +12,12 @@ const HomeScreen = () => {
       <Image
         style={{ width: 100, height: 100, resizeMode: "contain" }}
         source={logo}
+      />
+
+      <GooglePlacesAutocomplete
+        placeholder="Where from?"
+        nearbyPlacesAPI="GooglePlacesSearch"
+        debounce={400}
       />
 
       <NavOptions />
